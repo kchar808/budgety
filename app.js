4c9fa173d2695f8280fe75bbec23950f92ee7846
Keelana Char <keelanachar@gmail.com>
2020-03-31 19:32:19 -0500

    Clear fields after entering inputs. Put focus on description field.

--- app.js
+++ app.js
@@ -94,6 +94,20 @@ var UIController = (function() {
 
     },
 
+    clearFields: function() {
+      var fields, fieldsArray;
+      fields = document.querySelectorAll(`${DOMstrings.inputDescription}, ${DOMstrings.inputValue}`);
+
+      fieldsArray = Array.prototype.slice.call(fields);
+
+      fieldsArray.forEach(function(current, index, array) {
+        current.value = "";
+      });
+
+      fieldsArray[0].focus();
+    },
+
+
     getDOMStrings: function() {
       return DOMstrings;
     }
@@ -126,9 +140,12 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 3. Add the item to the UI
     UICtrl.addListItem(newItem, input.type);
 
-    // 4. Calculate the budget
+    // 4. Clear the fields
+    UICtrl.clearFields();
 
-    // 5. Display the budget
+    // 5. Calculate the budget
+
+    // 6. Display the budget
   };
 
   return {
